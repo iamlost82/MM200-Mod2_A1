56b83f3feaef5d4067649bc2a0ec2ee80ed9f682
Otto <otto.andre@deep-north.com>
2018-10-02 18:24:22 +0200

    Temporary disabled client routes except default "/"

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const userRoutes = require('./routes/api/userRoutes.js');
-const clientRoutes = require('./routes/client/clientRoutes.js')
+//const clientRoutes = require('./routes/client/clientRoutes.js')
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
@@ -12,5 +12,5 @@ app.listen(app.get('port'), function(){
     console.log('Your server is now running at port: ' + app.get('port'));
 });
 
-app.use('/users',userRoutes);
-app.use('/',clientRoutes);
+app.use('/api/users',userRoutes);
+//app.use('/',clientRoutes);
