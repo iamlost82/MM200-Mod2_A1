21006a835a87921409e84ebe8dc30c0e3451d762
Otto <otto.andre@deep-north.com>
2018-10-05 23:43:37 +0200

    updated server.js to handle jokes api endpoint

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const userRoutes = require('./routes/api/userRoutes.js');
-//const clientRoutes = require('./routes/client/clientRoutes.js')
+const jokeRoutes = require('./routes/api/jokeRoutes.js');
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
 app.use(bodyParser.json());
+
+app.use('/api/users',userRoutes);
+app.use('/api/joke',jokeRoutes);
+
 app.listen(app.get('port'), function(){
     console.log('Your server is now running at port: ' + app.get('port'));
 });
 
-app.use('/api/users',userRoutes);
-//app.use('/',clientRoutes);
