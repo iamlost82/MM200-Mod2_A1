5f95f2b26b29bb55fa35ef4f099011c565858a85
Otto <otto.andre@deep-north.com>
2018-10-06 13:43:01 +0200

    updated store endpoint to jokeroutes

--- routes/api/jokeRoutes.js
+++ routes/api/jokeRoutes.js
@@ -2,33 +2,26 @@ const express = require('express');
 const router = express.Router();
 const jokes = [
     {
-        id:"1",
         body:"<p>Mother: Anton, do you think I'm a bad mother?</p><p>You: My name is INPNAME!!</p>"
     },
     {
-        id:"2",
         body:"<p>Barista: How do you take your coffee?</p><p>INPNAME: Very, very seriously.</p>"
     },
     {
-        id:"3",
         body:"<p>And the lord said unto INPNAME, 'Come forth and you will receive eternal life'.</p><p>INPNAME came fifth and won a toaster.</p>"
     },
     {
-        id:"4",
         body: "<p>Dr. INPNAME: I'm sorry but you suffer from a terminal illness and have only 10 to live.</p><p>Patient: What do you mean, 10? 10 what? Months? Weeks?!</p><p>Dr INPNAME: Nine.</p>"
     },
     {
-        id:"5",
         body:"<p>INPNAME: CAPS LOCK – Preventing Login Since 1980.</p>"
     },
     {
-        id:"6",
         body:"<p>INPNAME: Tonight I dreamt of a beautiful walk on a sandy beach.</p><p>Better half: At least that explains the footprints I found in the cat litter box this morning.</p>"
     },
     {
-        id:"7",
         body:"<p>My friend INPNAME shocked and hurt me.</p><p>He told me today that I make people very uncomfortable and have no respect for personal space.</p><p>I mean, what a thing to say to a friend? It totally ruined our bath!</p>"
-    }
+    }    
 ];
 
 router.get('/', (req,res,next) => {
@@ -38,11 +31,22 @@ router.get('/', (req,res,next) => {
 });
 
 router.get('/random', (req,res,next) => {
-    let randomJoke = Math.floor(Math.random()*jokes.length);
-    let joke = jokes[randomJoke];
-    status = 200;
-    response = joke;
+    if(jokes.length > 0){
+        let randomJoke = Math.floor(Math.random()*jokes.length);
+        let joke = jokes[randomJoke];
+        status = 200;
+        response = joke;
+    } else{
+        status = 200;
+        response = {body:'<p>Sorry, no jokes currently stored on server...</p>'}
+    }
     res.status(status).json(response);
 });
+
+router.post('/new', (req,res,next) => {
+    let newJoke = req.body
+    jokes.push(newJoke);
+    res.status(201).json({msg:"New joke created"});
+})
 
 module.exports = router;
